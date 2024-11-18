e2d2d968732b9eee3458713d9ce2fd672389c8b0
reymezis <qq1s@bk.ru>
2024-11-18 16:24:34 +0700

    add feeds and posts

--- src/app.js
+++ src/app.js
@@ -1,8 +1,46 @@
 import * as yup from 'yup';
 import i18next from 'i18next';
 import onChange from 'on-change';
+import axios from 'axios';
+import _ from 'lodash';
 import render from './view.js';
 import resources from './locales/ru.js';
+
+const getParsedData = (response) => {
+  const parser = new DOMParser();
+  const data = response.data.contents;
+  const parsedData = parser.parseFromString(data, 'application/xml');
+  return parsedData;
+};
+
+const getTagContent = (node) => (node.textContent.trim());
+
+const buildPosts = (items, feedId) => {
+  const posts = [];
+  items.forEach((item) => {
+    const post = {
+      feedId,
+      postId: _.uniqueId(),
+      title: '',
+      link: '',
+      description: '',
+    };
+    Array.from(item.children)
+      .forEach((el) => {
+        if (el.tagName === 'title') {
+          post.title = getTagContent(el);
+        }
+        if (el.tagName === 'link') {
+          post.link = getTagContent(el);
+        }
+        if (el.tagName === 'description') {
+          post.description = getTagContent(el);
+        }
+      });
+    posts.push(post);
+  });
+  return posts;
+};
 
 export default async () => {
   // MODEL
@@ -10,12 +48,17 @@ export default async () => {
     formState: null,
     error: null,
     rssList: [],
+    posts: [],
+    feeds: [],
+    modal: null,
   };
 
   const elements = {
     form: document.querySelector('.rss-form'),
     input: document.getElementById('url-input'),
     feedback: document.querySelector('.feedback'),
+    feeds: document.querySelector('.feeds'),
+    posts: document.querySelector('.posts'),
   };
 
   const defaultLanguage = 'ru';
@@ -40,7 +83,7 @@ export default async () => {
     });
 
   // VIEW
-  const state = onChange(initialState, render(elements, i18nInstance));
+  const state = onChange(initialState, render(elements, i18nInstance, initialState));
 
   // CONTROLLER
   elements.form.addEventListener('submit', (e) => {
@@ -60,11 +103,39 @@ export default async () => {
         state.formState = 'valid';
         state.error = null;
         state.rssList.push(value.url);
+        axios.get(`https://allorigins.hexlet.app/get?disableCache=true&url=${encodeURIComponent(enteredValue)}`)
+          .then((response) => {
+            const parsed = getParsedData(response);
+            const content = parsed.querySelector('channel').children;
+            const [title, description, ...items] = content;
+            const feedId = _.uniqueId();
+            const feedObj = {
+              feedId,
+              title: getTagContent(title),
+              description: getTagContent(description),
+            };
+            state.feeds.push(feedObj);
+            const postsItems = items.filter((el) => el.tagName === 'item');
+            const posts = buildPosts(postsItems, feedId);
+            state.posts = [...state.posts, ...posts];
+          })
+          .catch((err) => {
+            throw new Error(err);
+          });
       })
       .catch((err) => {
         const errObj = err.message;
         state.error = errObj.key;
         state.formState = 'invalid';
       });
+  });
+
+  elements.posts.addEventListener('click', (e) => {
+    e.preventDefault();
+    const element = e.target;
+    if (element.matches('button')) {
+      const id = element.getAttribute('data-id');
+      state.modal = id;
+    }
   });
 };
--- src/view.js
+++ src/view.js
@@ -23,7 +23,65 @@ const handleProcessError = (elements, value, i18nInstance) => {
   }
 };
 
-export default (elements, i18nInstance) => (path, value, previousValue) => {
+const handleProcessFeeds = (elements, initialState) => {
+  elements.feeds.textContent = '';
+  const feedsContent = document.createElement('div');
+  elements.feeds.append(feedsContent);
+  feedsContent.outerHTML = `<div class="card border-0">
+        <div class="card-body">
+          <h2 class="card-title h4">Фиды</h2>
+        </div>
+      <ul class="list-group border-0 rounded-0 list-unstyled"></ul>
+    </div>`;
+  const ul = document.querySelector('.feeds > div > ul');
+  initialState.feeds.forEach(({ title, description }) => {
+    const li = document.createElement('li');
+    ul.append(li);
+    li.outerHTML = `<li class="list-group-item border-0 border-end-0">
+          <h3 class="h6 m-0">${title}</h3>
+          <p class="m-0 small text-black-50">${description}</p>
+        </li>`;
+  });
+  elements.form.reset();
+  elements.input.focus();
+};
+
+const handleProcessPosts = (elements, initialState) => {
+  elements.posts.textContent = '';
+  const postsContent = document.createElement('div');
+  elements.posts.append(postsContent);
+  postsContent.outerHTML = `<div class="card border-0">
+    <div class="card-body">
+      <h2 class="card-title h4">Посты</h2>
+    </div>
+    <ul class="list-group border-0 rounded-0 list-unstyled"></ul>
+  </div>`;
+  const ul = document.querySelector('.posts > div > ul');
+  initialState.posts.forEach(({ postId, title, link }) => {
+    const li = document.createElement('li');
+    ul.append(li);
+    li.outerHTML = `<li class="list-group-item d-flex justify-content-between align-items-start border-0 border-end-0">
+      <a href=${link} class="fw-bold" data-id=${postId} target="_blank" rel="noopener noreferrer">${title}</a>
+      <button type="button" class="btn btn-outline-primary btn-sm" data-id=${postId} data-bs-toggle="modal" data-bs-target="#modal">Просмотр</button>
+    </li>`;
+  });
+  elements.form.reset();
+  elements.input.focus();
+};
+
+const handleProcessModal = (value, initialState) => {
+  const modal = document.querySelector('.modal');
+  const currentModalInfo = initialState.posts.filter(({ postId }) => postId === value);
+  const [{ title, link, description }] = currentModalInfo;
+  const modalTitle = modal.querySelector('.modal-title');
+  modalTitle.textContent = title;
+  const modalBody = modal.querySelector('.modal-body');
+  modalBody.textContent = description;
+  const linkModal = modal.querySelector('.modal-footer > a');
+  linkModal.setAttribute('href', link);
+};
+
+export default (elements, i18nInstance, initialState) => (path, value, previousValue) => {
   switch (path) {
     case 'formState':
       handleProcessState(elements, value, previousValue, i18nInstance);
@@ -31,6 +89,18 @@ export default (elements, i18nInstance) => (path, value, previousValue) => {
 
     case 'error':
       handleProcessError(elements, value, i18nInstance);
+      break;
+
+    case 'feeds':
+      handleProcessFeeds(elements, initialState);
+      break;
+
+    case 'posts':
+      handleProcessPosts(elements, initialState);
+      break;
+
+    case 'modal':
+      handleProcessModal(value, initialState);
       break;
 
     default:
